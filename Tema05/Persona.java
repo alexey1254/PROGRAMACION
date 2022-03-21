879fbcd7547050a302a83d4714e28217dd865a17
Alejandro Marrero Cabrera <44745574+alexey1254@users.noreply.github.com>
2022-03-21 18:28:53 +0000

    Añadido el toString

--- Tema05/Persona.java
+++ Tema05/Persona.java
@@ -1,19 +1,31 @@
 package tema05;
 
 public class Persona {
-  private String nombre, apellidos;
-  private int edad;
-  
-  public void setEdad(int edad) {
-    this.edad=edad;
-  }
-  
-  public void setNombre(String nom) {
-   nombre=nom; 
-  }
-  
-  @Override
-  public boolean equals(Object p) {
-   return this.nombre.equals( ((Persona) p).nombre) && this.edad== ((Persona)p).edad)); 
-  }
+	private String nombre, apellidos;
+	private int edad;
+	
+	public Persona(String nombre, String apellidos, int edad) {
+	this.nombre=nombre;
+	this.apellidos=apellidos;
+	this.edad=edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+
+	public void setNombre(String nom) {
+		nombre=nom; 
+	}
+
+	@Override
+	public boolean equals(Object p) {
+		return this.nombre.equals( ((Persona) p).nombre) && this.edad== ((Persona)p).edad)); 
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("Nombre: %s, Apellidos: %s, Edad: %d, IdObjeto: %s.", this.nombre, this.apellidos, this.edad, super.toString());	
+	}
 }
