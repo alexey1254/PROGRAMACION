5f4e10390edc81bef4b55e488e437cca6e321a3f
alexey1254 <alxkill54@gmail.com>
2022-04-13 11:36:15 +0100

    Se añade el ejercicio 7 y se termina

---
+++ Tema05/src/tema05/Ejercicios/ejercicio07/FichaDomino.java
@@ -0,0 +1,64 @@
+package tema05.Ejercicios.ejercicio07;
+
+public class FichaDomino {
+    private int ficha[] = new int[2];
+
+    /**
+     * Inicializa una ficha de dominó
+     * 
+     * @param izqda Numero que va a la izquierda
+     * @param dcha  Numero que va a la derecha
+     */
+    public FichaDomino(int izqda, int dcha) {
+        this.ficha[0] = izqda;
+        this.ficha[1] = dcha;
+    }
+
+    /**
+     * Voltea la ficha de domino
+     * 
+     */
+    public FichaDomino voltea() {
+        return new FichaDomino(this.ficha[1], this.ficha[0]);
+    }
+
+    /**
+     * Revisa si las fichas encajan
+     * 
+     * @param objeto FichaDomino
+     * @return True si encaja
+     */
+    public boolean encaja(FichaDomino objeto) {
+        return this.ficha[0] == objeto.ficha[0] || this.ficha[0] == objeto.ficha[1] || this.ficha[1] == objeto.ficha[0]
+                || this.ficha[1] == objeto.ficha[1];
+    }
+
+    @Override
+    public String toString() {
+        if (this.ficha[0] == 0 && this.ficha[1] == 0) {
+            return "[  |  ]";
+        } else if (this.ficha[0] == 0) {
+            return String.format("[  | %s ]", this.ficha[1]);
+        } else if (this.ficha[1] == 0) {
+            return String.format("[ %s |  ]", this.ficha[0]);
+        } else {
+            return String.format("[ %s | %s ]", this.ficha[0], this.ficha[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        FichaDomino f1 = new FichaDomino(6, 1);
+        FichaDomino f2 = new FichaDomino(0, 4);
+        FichaDomino f3 = new FichaDomino(3, 3);
+        FichaDomino f4 = new FichaDomino(0, 1);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+        System.out.println(f4);
+        System.out.println(f2.voltea());
+        System.out.println(f2.encaja(f4));
+        System.out.println(f1.encaja(f4));
+        System.out.println(f1.encaja(f3));
+        System.out.println(f1.encaja(f2));
+    }
+}
