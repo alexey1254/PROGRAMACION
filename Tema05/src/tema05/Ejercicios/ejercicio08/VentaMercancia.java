ddd89f8649ac560ba4e4dae682fe6e4258e539cf
alexey1254 <alxkill54@gmail.com>
2022-04-21 17:36:58 +0100

    Se corrige el ejercicio VentaMercancia

--- Tema05/src/tema05/Ejercicios/ejercicio08/VentaMercancia.java
+++ Tema05/src/tema05/Ejercicios/ejercicio08/VentaMercancia.java
@@ -5,46 +5,99 @@ import java.util.*;
 import java.io.*;
 
 public class VentaMercancia {
-    Map<String, Double> producto = new LinkedHashMap<String, Double>();
-    Map<String, Integer> compraCliente = new HashMap<String, Integer>();
-    String rutaProductos = "C:\\Users\\aleco\\Desktop\\GitHub\\PROGRAMACION\\Tema05\\src\\tema05\\Ejercicios\\ejercicio08\\T05-E08-DatosProductosSupermercado.txt";
-    String rutaCompraCliente = "C:\\Users\\aleco\\Desktop\\GitHub\\PROGRAMACION\\Tema05\\src\\tema05\\Ejercicios\\ejercicio08\\T05-E08-DatosCompraCliente.txt";
-
+    private Map<String,Double> productos = new HashMap<>();
+    private Map<String,Integer> cesta = new HashMap<>();
+    private boolean hayDescuento = false;
+    
     /**
-     * Implementa al comercio los productos junto con su precio en un Mapa
-     * 
+     * VentaMercancia
+     * Lee los productos y sus precios
+     * @param ruta
      * @throws FileNotFoundException
      */
-    public void implementarProductos() throws FileNotFoundException {
-        File archivo = new File(rutaProductos);
-        Scanner leerDatos = new Scanner(archivo);
-        while (leerDatos.hasNextLine()) {
-            String clave = leerDatos.next(); // ! Esta linea da problemas
-            double precio = leerDatos.nextDouble();
-            producto.put(clave, precio);
-            leerDatos.nextLine();
+    public void leerProductos(String ruta) throws FileNotFoundException {
+        Scanner fDatos = new Scanner(new File(ruta));
+        String linea, lineaDatos[];
+        String producto;
+        double precio;
+        while(fDatos.hasNextLine()) {
+            linea = fDatos.nextLine();
+            linea = linea.replace(",",".");
+            lineaDatos = linea.split(" ");
+            producto = lineaDatos[0];
+            precio = Double.parseDouble(lineaDatos[1]);
+            productos.put(producto, precio);
         }
-        leerDatos.close();
     }
 
     /**
-     * Lee los datos de la compra del cliente desde un archivo de texto externo
-     * 
+     * VentaMercancia
+     * Lee la cesta de la compra
+     * @param ruta
      * @throws FileNotFoundException
      */
-    public void datosCompraCliente() throws FileNotFoundException {
-        File archivo = new File(rutaCompraCliente);
-        Scanner leerDatos = new Scanner(archivo);
-        while (leerDatos.hasNextLine()) {
-            compraCliente.put(leerDatos.next(), leerDatos.nextInt());
-            leerDatos.nextLine();
+    public void leerCesta(String ruta) throws FileNotFoundException {
+        Scanner fDatos = new Scanner(new File(ruta));
+        String linea, lineaDatos[];
+        String producto;
+        int cantidad;
+        while(fDatos.hasNextLine()) {
+            linea = fDatos.nextLine();
+            lineaDatos = linea.split(" ");
+            producto = lineaDatos[0];
+            cantidad = Integer.parseInt(lineaDatos[1]);
+            if(cesta.containsKey(producto)) {
+                cesta.put(producto, cesta.get(producto) + cantidad);
+            } else {
+                cesta.put(producto, cantidad);
+            }
         }
-        leerDatos.close();
+    }
+
+    
+    public static boolean verificarDescuento(String codigoDescuento) {
+        return codigoDescuento.equals("ECODTO");
+    }
+
+    public void imprimirTicket(double descuento) {
+        System.out.println("" +
+        "-----------  ------    --------    --------\n" +
+        "Producto     Precio    Cantidad    Subtotal\n" +
+        "-----------  ------    --------    --------\n");
+
+        double precio, total = 0;
+        int cantidad;
+        for(String producto: this.cesta.keySet()) {
+            precio = this.productos.get(producto);
+            cantidad = this.cesta.get(producto);
+            total += precio * cantidad;
+            if(cantidad > 1) {
+                System.out.printf("%-11s %6.2f %8d %8f",producto, precio, cantidad, cantidad*precio );
+            } else {
+                System.out.printf("%-27s %8f\n", producto, precio);
+            }
+        }
+        System.out.println("-----------  ------    --------    --------\n");
+        if(hayDescuento) {
+            System.out.printf("%-27s %f","Descuento\n", (descuento * total/100) );
+            System.out.println("-----------  ------    --------    --------\n");
+        } else {
+            System.out.printf("%-27s: %f","Total:\n", total );
+            System.out.println("-----------  ------    --------    --------\n");
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner teclado = new Scanner(System.in);
         VentaMercancia v = new VentaMercancia();
-        v.implementarProductos();
-        v.datosCompraCliente();
+        v.leerProductos("/home/daw/Escritorio/GitHub/PROGRAMACION/Tema05/src/tema05/Ejercicios/ejercicio08/T05-E08-DatosProductosSupermercado.txt");
+        v.leerCesta("/home/daw/Escritorio/GitHub/PROGRAMACION/Tema05/src/tema05/Ejercicios/ejercicio08/T05-E08-DatosCompraCliente.txt");
+        System.out.println("Introduce un codigo de descuento: ");
+        String descuentoUsuario = teclado.nextLine();
+        if(verificarDescuento(descuentoUsuario)) {
+            v.imprimirTicket(10);
+        } else {
+            v.imprimirTicket(0);
+        }
     }
 }
